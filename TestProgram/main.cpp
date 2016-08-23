dc028a8c3576f7a6a842928d3850219a72e92074
trevor.liloia <trevor.liloia@students.aie.edu.au>
2016-08-23 11:39:20 -0700

    chatterbox v0.2.0

--- TestProgram/main.cpp
+++ TestProgram/main.cpp
@@ -1,7 +1,8 @@
 ﻿#include <iostream>
+#include <stdio.h>
+#include <cstdio>
 #include <string>
 #include <time.h>
-
 using namespace std;
 int main()
 {
@@ -12,32 +13,37 @@ int main()
 	char menuitem;
 	cout << "Please use lowercase and simple words/concepts when talking to Chatterbox!\n\n";
 	cout << "\nHi! I'm Chatterbox!\n";
-	cin >> answer1;
+	
 	while(understood == false)
 	{
-		if (answer1 == "hi" || "hello" || "hey")
+		cin >> answer1;
+		if (answer1 == "hi" || answer1 == "hello")
 		{
 			understood = true;
 		}
 		else
 		{
-			cout << "I'm sorry, I didn't understand that.";
-			
+			cout << "I'm sorry, I didn't understand that.\n";
+		}
+	}
+	understood = false;
+	cout << "\nWhat is your name?\n";
+	cin >> name;
+
+	cout << "\nHi " + name + ", how are you today?.\n";
+
+	while (understood == false)
+	{
+		cin >> answer1;
+		if (answer1 == "good" || answer1 == "great" || answer1 == "awesome" || answer1 == "super")
+		{
+
+		}
+		else if (answer1 == "okay" || answer1 == "fine" || answer1 == "alright")
+		{
+
 		}
 	}
 	
-	cout << "\nWhat is your name?\n";
-	cin >> name;
-	cout << "\nHi " + name + ", how are you today?.\n";
-	cin >> answer1;
-	if (answer1 == "good" || "great" || "awesome" || "super")
-	{
-
-	}
-	else if (answer1 == "okay" || "fine" || "alright")
-	{
-
-	}
-
 	system("pause");
 }
