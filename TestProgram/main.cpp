11ae2bbc9d3e66819e8389dd8877d84d20e70b29
trevor.liloia <trevor.liloia@students.aie.edu.au>
2016-08-23 12:04:41 -0700

    chatter v02.1.1

--- TestProgram/main.cpp
+++ TestProgram/main.cpp
@@ -2,11 +2,17 @@
 #include <stdio.h>
 #include <cstdio>
 #include <string>
-#include <time.h>
 using namespace std;
+
+void NotUnderstood()
+{
+	cout << "I'm sorry, I don't understand. \nI am still in development stages,\nand it's hard to learn new words.\n";
+}
+
 int main()
 {
 	bool understood = false;
+	int option;
 	int answer1num;
 	string answer1;
 	string name;
@@ -23,7 +29,7 @@ int main()
 		}
 		else
 		{
-			cout << "I'm sorry, I didn't understand that.\n";
+			NotUnderstood();
 		}
 	}
 	understood = false;
@@ -35,15 +41,43 @@ int main()
 	while (understood == false)
 	{
 		cin >> answer1;
-		if (answer1 == "good" || answer1 == "great" || answer1 == "awesome" || answer1 == "super")
+		if (answer1 == "good" || answer1 == "great" || answer1 == "awesome" || answer1 == "super" || answer1 == "dandy" || answer1 == "wonderful" || answer1 == "spectacular")
 		{
-
+			option = 1;
+			understood == true;
 		}
 		else if (answer1 == "okay" || answer1 == "fine" || answer1 == "alright")
 		{
-
+			option = 2;
+			understood == true;
+		}
+		else if (answer1 == "bad" || answer1 == "not good" || answer1 == "terrible" || answer1 == "horrible" || answer1 == "frustrated" || answer1 == "mad")
+		{
+			option = 3;
+			understood == true;
+		}
+		else if (answer1 == "no how are you" || answer1 == "how are you" || answer1 == "no how are you today" || answer1 == "how are you today")
+		{
+			cout << "No, silly, I'm asking you!";
+		}
+		else
+		{
+			NotUnderstood;
 		}
 	}
-	
+	understood = false;
+
+	switch (option)
+	{
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		default:
+			break;
+	}
 	system("pause");
 }
+
